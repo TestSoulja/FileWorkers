fa22f3672e90a1c17cb40a449f9afae6b089a81e
TestSoulja <serzh.ekcker@yandex.ru>
2024-09-20 21:19:26 +0300

    modified:   DeleteKeys/main.py 	modified:   ReadMe.md 	modified:   Some_from_json/ewq.py 	new file:   Workinfile/main.py

---
+++ Workinfile/main.py
@@ -0,0 +1,48 @@
+import os
+
+text = 'tags: \n  - gamesdb' # фраза, которая будет дописана в конец строки
+output = '' # инициализация результирующего текста
+marker = 0
+
+directory = "C:/Users\TeSoul\Documents\Obsidian Vault\Trash\Movie list"
+ 
+# Получаем список файлов
+files = os.listdir(directory)
+
+def changein():
+    output = ''
+    for f in files:
+        with open(directory + '/' + f, 'r', encoding="utf-8") as file:
+            for line in file:
+                if "gamesdb" in line:
+                    output += line.replace("gamesdb", "moviedb")
+                else:
+                    output += line
+
+        with open(directory + '/' + f, 'w', encoding="utf-8") as file:
+                file.write(output)
+    output = ''
+
+def addin():
+    # Выводим список файлов
+    for f in files:
+        with open(directory + '/' + f, 'r', encoding="utf-8") as file:
+            for line in file: # считывание текущего файла
+                if "tags" in line:
+                    marker = 1
+
+        if marker == 0:
+            with open(directory + '/' + f, 'r', encoding="utf-8") as file:
+                for line in file: # считывание текущего файла
+                    if "Status" in line:
+                        output += (text + '\n')
+                    output += line
+        if marker == 0:
+            with open(directory + '/' + f, 'w', encoding="utf-8") as file:
+                file.write(output)
+
+        output = '' # инициализация результирующего текста
+        marker = 0
+        
+# changein()
+# addin()
